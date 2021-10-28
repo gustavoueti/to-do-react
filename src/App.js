e81df56cefadc2d70e8d057e2274f6dfb00d13d7
Gustavo Ueti <gustavo.ueti@gmail.com.br>
2021-10-28 17:03:03 -0300

    feat: :art: add useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react"
+import React, { useState } from "react"
 import "./App.css";
 //Components
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("")
   return (
     <div className="App">
       <header>
